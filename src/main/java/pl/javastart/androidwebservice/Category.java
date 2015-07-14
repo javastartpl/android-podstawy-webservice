df2b5aa1e8d2b4d6de733485c6c43ccae4f55dcd
Marcin Kunert <the.martines@gmail.com>
2015-07-14 17:03:04 +0200

    Minor fixes

--- src/main/java/pl/javastart/androidwebservice/Category.java
+++ src/main/java/pl/javastart/androidwebservice/Category.java
@@ -10,17 +10,9 @@ public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
+    private long id;
 
     private String name;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
