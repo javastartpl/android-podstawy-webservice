9cbbd82ff836f51b13009a93a6e37f89f933a690
Marcin Kunert <the.martines@gmail.com>
2018-09-11 14:11:03 +0200

    Wersja 1.1

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 ./mvnw clean package
 docker build -t android-podstawy-webservice .
-docker tag android-podstawy-webservice javastartpl/android-podstawy-webservice:1.0
-docker push javastartpl/android-podstawy-webservice:1.0
+docker tag android-podstawy-webservice javastartpl/android-podstawy-webservice:1.1
+docker push javastartpl/android-podstawy-webservice:1.1
